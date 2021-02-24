f0d1ccc8c03e4dfd3ff1400f317a81b86ebcb9ce
Sofant <alu0101206011@ull.edu.es>
2021-02-24 11:41:27 +0000

    Funciona gtkwave

--- mul.v
+++ mul.v
@@ -4,8 +4,8 @@
 
 module multiplicador(input wire [2:0] multiplicando, input wire [2:0] multiplicador, input wire clk, reset, output wire [5:0] result, output wire Fin);
   wire Carga_A, Carga_QM, Desplaza_AQ, Reset_A, Resta, q0, q_menos1;
-
-  uc unidad_control(q0, reset, clk, q_menos1, Carga_A, Carga_QM, Desplaza_AQ, Reset_A, Resta, Fin); 
+ 
   cd camino(multiplicador, multiplicando, Carga_A, Carga_QM, Desplaza_AQ, Reset_A, Resta, clk, reset, q0, q_menos1, result);   
+  uc unidad_control(q0, reset, clk, q_menos1, Carga_A, Carga_QM, Desplaza_AQ, Reset_A, Resta, Fin);
 
 endmodule
